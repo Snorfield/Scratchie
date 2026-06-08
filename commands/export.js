eba6c2fefe1429c973f84b966eef79fd20f67f15
S. Norfield <225273460+Snorfield@users.noreply.github.com>
2026-06-08 10:13:49 -0600

    Update export.js

--- commands/export.js
+++ commands/export.js
@@ -1,9 +1,11 @@
 const project = require('./project');
 const ping = require('./ping');
 const profile = require('./profile');
+const studio = require('./studio');
 
 module.exports = {
     ping,
     project,
-    profile
+    profile,
+    studio
 }
