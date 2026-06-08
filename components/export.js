ef53a203d987c33e0c53324d20209f845acbf285
S. Norfield <225273460+Snorfield@users.noreply.github.com>
2026-06-07 22:37:31 -0600

    Create export.js to bundle module exports

---
+++ components/export.js
@@ -0,0 +1,9 @@
+const container = require('./container');
+const project = require('./project');
+const profile = require('./profile');
+
+module.exports = {
+    container, 
+    project,
+    profile
+}
