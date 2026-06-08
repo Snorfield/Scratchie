9f8004dd10ec2fcad47e8b39490659a385e6da9b
S. Norfield <225273460+Snorfield@users.noreply.github.com>
2026-06-08 10:12:54 -0600

    Update message.js

--- handlers/message.js
+++ handlers/message.js
@@ -25,7 +25,7 @@ async function linkProfile(message) {
  */
 
 async function linkProject(message) {
-	const matches = (message.content).match(/scratch\.mit\.edu\/projects\/([0-9]+)/);
+    const matches = (message.content).match(/scratch\.mit\.edu\/projects\/([0-9]+)/);
     if (matches) {
         const id = matches[1];
 
@@ -38,30 +38,56 @@ async function linkProject(message) {
 }
 
 /**
+ * Capture Scratch studio links and send a preview of them
+ * @param {object} message 
+ */
+
+async function linkStudio(message) {
+    const matches = (message.content).match(/scratch\.mit\.edu\/studios\/([0-9]+)/);
+    if (matches) {
+        const id = matches[1];
+
+        const information = await get(`https://api.scratch.mit.edu/studios/${id}`);
+
+        if (information) {
+            message.reply(components.studio(information));
+        }
+    }
+}
+
+/**
  * Capture Scratch profile and project links to warn the user not to advertise
  * @param {object} message 
  */
 
 function captureLinks(message) {
     const userLink = /https:\/\/scratch\.mit\.edu\/users\/\S+/g;
-	const projectLink = /https:\/\/scratch\.mit\.edu\/projects\/\S+/g;
+    const projectLink = /https:\/\/scratch\.mit\.edu\/projects\/\S+/g;
+    const studioLink = /https:\/\/scratch\.mit\.edu\/studios\/\S+/g;
 
-	if (userLink.test(message.content)) {
-		message.reply(components.container(
-			"Hey, please don't advertise user profiles!\n-# If you weren't you can ignore this.",
-			16756224
-		));
-	}
-	if (projectLink.test(message.content)) {
-		message.reply(components.container(
-			"Hey, please don't advertise projects!\n-# If you weren't you can ignore this.",
-			16756224
-		));
-	}
+    if (userLink.test(message.content)) {
+        message.reply(components.container(
+            `Hey <@${message.author.id}>, please don't advertise user profiles!\n-# If you weren't you can ignore this.`,
+            16756224
+        ));
+    }
+    if (projectLink.test(message.content)) {
+        message.reply(components.container(
+            `Hey <@${message.author.id}>, please don't advertise projects!\n-# If you weren't you can ignore this.`,
+            16756224
+        ));
+    }
+    if (studioLink.test(message.content)) {
+        message.reply(components.container(
+            `Hey <@${message.author.id}>, please don't advertise studios!\n-# If you weren't you can ignore this.`,
+            16756224
+        ));
+    }
 }
 
 module.exports = [
     linkProfile,
-	linkProject,
+    linkProject,
+    linkStudio,
     captureLinks
 ]
