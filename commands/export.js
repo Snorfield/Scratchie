ffcb18592257c58dbcf421bac9486d11bcec3a22
S. Norfield <225273460+Snorfield@users.noreply.github.com>
2026-06-09 12:25:00 -0600

    Add explore module to export list

--- commands/export.js
+++ commands/export.js
@@ -2,10 +2,12 @@ const project = require('./project');
 const ping = require('./ping');
 const profile = require('./profile');
 const studio = require('./studio');
+const explore = require('./explore');
 
 module.exports = {
     ping,
     project,
     profile,
-    studio
+    studio,
+    explore
 }
