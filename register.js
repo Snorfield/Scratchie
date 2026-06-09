d58d4c6035fad37d360ad9893cbe657fbd0aa370
John <roblox888i12345@gmail.com>
2026-06-09 18:43:55 -0400

    feat: Update slash commands to include news command

--- register.js
+++ register.js
@@ -11,6 +11,15 @@ const commands = [
             InteractionContextType.PrivateChannel
         ])
         .toJSON(),
+    new SlashCommandBuilder()
+        .setName('news')
+        .setDescription('Fetch news from the Scratch Homepage')
+        .setContects([
+            InteractionContextType.Guild,
+            InteractionContextType.BotDM,
+            InteractionContextType.PrivateChannel
+            ])
+         .toJSON(),
 
     new SlashCommandBuilder()
         .setName('explore')
