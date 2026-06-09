b4ece9764fa9f535b3388f00a809a0d539ba9dc2
S. Norfield <225273460+Snorfield@users.noreply.github.com>
2026-06-09 17:58:51 -0600

    Update news.js

--- commands/news.js
+++ commands/news.js
@@ -3,23 +3,66 @@ const components = require('../components/export');
 
 async function news(interaction) {
     await interaction.deferReply();
-    try {
-        const information = await get('https://api.scratch.mit.edu/news?limit=3');
-    
-        if (information && response.length > 0) {
-            const newsList = response.map(function(item) {
-                return {
-                    headline: item.headline,
-                    copy: item.copy
-                };
-            });
-            return interaction.editReply(components.news(newsList));
-        } else {
-            throw new Error("500: News might be down from the API");
-        }    
-    } catch (error) {
+
+    const information = await get('https://api.scratch.mit.edu/news?limit=3');
+
+    if (information) {
+
+        const container = {
+            type: 17,
+            accent_color: 16756224,
+            spoiler: false,
+            components: []
+        };
+
+        for (const article of information) {
+            container.components.push(
+                {
+                    type: 9,
+                    accessory: {
+                        type: 11,
+                        media: {
+                            url: article.image
+                        },
+                        description: null,
+                        spoiler: false
+                    },
+                    components: [
+                        {
+                            type: 10,
+                            content: `## ${article.headline}\n${article.copy}`
+                        }
+                    ]
+                },
+                {
+                    type: 1,
+                    components: [
+                        {
+                            type: 2,
+                            style: 5,
+                            url: article.url,
+                            label: "Article",
+                            disabled: false
+                        }
+                    ]
+                },
+                {
+                    type: 14
+                }
+            );
+        }
+
+        if (container.components.at(-1)?.type === 14) {
+            container.components.pop();
+        }
+
+        return interaction.editReply({
+            components: [container],
+            flags: 32768
+        });
+    } else {
         return interaction.editReply(components.container(
-            "Error while fetching news information",
+            "Error while fetching news information!",
             16756224
         ));
     }
