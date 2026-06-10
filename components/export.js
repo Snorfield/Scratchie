f775525a440022525cca91fd4a411a0f7a764d08
S. Norfield <225273460+Snorfield@users.noreply.github.com>
2026-06-09 19:54:03 -0600

    Update export.js

--- components/export.js
+++ components/export.js
@@ -2,12 +2,10 @@ const container = require('./container');
 const project = require('./project');
 const profile = require('./profile');
 const studio = require('./studio');
-const news = require('./news');
 
 module.exports = {
     container, 
     project,
     profile,
-    studio,
-    news
+    studio
 }
