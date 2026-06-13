5870f919a66bb95ef27a641a5b664ad69c3596ac
John <roblox888i12345@gmail.com>
2026-06-12 20:02:42 -0400

    Refactor commands function for improved clarity

--- commands/commands.js
+++ commands/commands.js
@@ -3,39 +3,37 @@ const get = require('../functions/fetch');
 const components = require('../components/export');
 const commandsData = require('../data/commands.json');
 
-
 async function commands(interaction) {
-    await interaction.deferReply();
+   await interaction.deferReply();
 
-    let body = '# Commands\n';
-    const keys = Object.keys(commandsData);
-    
-    for (let i = 0; i < keys.length; i++) {
-        const data = commandsData[keys[i]];
-        body += `### ${data.name}\n${data.description}\n-# Params: ${data.params}\n-# Credits: ${data.credits}`;
-        if (!(i === keys.length - 1)) {
-            body += '\n';
-        }
-    }
+   let body = '# Commands\n';
+   const keys = Object.keys(commandsData);
+  
+   for (let i = 0; i < keys.length; i++) {
+       const data = commandsData[keys[i]];
+       body += `### ${data.name}\n${data.description}\n-# Params: ${data.params}\n-# Credits: ${data.credits}`;
+       if (!(i === keys.length - 1)) {
+           body += '\n';
+       }
+   }
 
-    if (body) {
-   return {
-        components: [
-            new ContainerBuilder()
-                .setAccentColor(16756224)
-                .addTextDisplayComponents(
-                    new TextDisplayBuilder().setContent(body)
-                    )
-        ]
-
-      flags: MessageFlags.IsComponentsV2
-
-    }
-    } else {
-        return interaction.editReply(components.container(
-            "Error while fetching command information!",
-            16756224
-        ));
+   if (body) {
+       return await interaction.editReply({
+           components: [
+               new ContainerBuilder()
+                   .setAccentColor(16756224)
+                   .addTextDisplayComponents(
+                       new TextDisplayBuilder().setContent(body)
+                   )
+           ],
+           flags: MessageFlags.IsComponentsV2
+       });
+   } else {
+       return await interaction.editReply(components.container(
+           "Error while fetching command information!",
+           16756224
+       ));
+   }
 }
 
 module.exports = commands;
