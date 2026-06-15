f7b52cbed8001712778b2eba4e6e21811b93a861
John <roblox888i12345@gmail.com>
2026-06-15 16:45:12 -0400

    Remove news from module exports in export.js
    Removed 'news' from the exported module in export.js.

--- commands/export.js
+++ commands/export.js
@@ -3,13 +3,11 @@ const ping = require('./ping');
 const profile = require('./profile');
 const studio = require('./studio');
 const explore = require('./explore');
-const news = require('./news');
 
 module.exports = {
     ping,
     project,
     profile,
     studio,
-    explore,
-    news
+    explore
 }
